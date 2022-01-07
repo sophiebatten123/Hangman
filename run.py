0937805582aea7ce4ce6a699520605d91bf611c3
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-01-07 09:50:22 +0000

    doctype has been added to document the purpose of this function

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ from words import word_list
 import random
 
 def get_word():
+    """
+    This function generates a random word from the list 
+    it then returns the variable once it has been ran
+    """
     word = random.choice(word_list)
     print(word)
     return word
-
-get_word()
